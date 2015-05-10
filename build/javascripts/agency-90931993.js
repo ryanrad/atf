f2a3670f559c8aee7a7487e07fdebdc15e6f22b4
Ryan Radaszewski <ryan40+github@gmail.com>
2015-05-09 22:52:13 -0400

    Push up previously gitignored /build directory.

---
+++ build/javascripts/agency-90931993.js
@@ -0,0 +1,6 @@
+/*!
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+$(".navbar-collapse ul li a").click(function(){$(".navbar-toggle:visible").click()});
